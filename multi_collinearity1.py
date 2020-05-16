498bc68829af311fb1b6267c571b525c911f0ef4
Yasuhiko Tanaka <tanaka.yasuhiko@leadinge.co.jp>
2020-05-16 20:19:48 +0900

    元データ（訓練データ全部）の多重共線性のチェック用

---
+++ multi_collinearity1.py
@@ -0,0 +1,20 @@
+"""
+　多重共線性のチェック：対象は説明変数の候補全部
+"""
+
+# ライブラリのインポート
+import pandas as pd
+
+# 定数の初期化
+data_folder = '.\\data\\'
+train_file_name = 'train_org.csv'
+
+train_file_path = data_folder + train_file_name
+
+# 訓練データのロード
+train = pd.read_csv(train_file_path, encoding='UTF8')
+
+# 多重共線性確認
+df1 = pd.DataFrame(train)
+df2 = df1.drop('y', axis=1)
+print(df2.corr())
